717345ab103dc146ff7c51eb94f9723071f282b7
Jossueth <jossuetheldomador@hotmail.com>
2022-10-26 23:16:02 -0500

    Finalizando funcionalidad de completar

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+( () => {
+
 const btn = document.querySelector("[data-form-btn]");
 
 const crearTarea = (evento) => {
@@ -6,15 +8,37 @@ const crearTarea = (evento) => {
     const list = document.querySelector('[data-list]');
     const task = document.createElement('li');
     task.classList.add('card');
-    const content = `<div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${input.value}</span>
-  </div>
-  <i class="fas fa-trash-alt trashIcon icon"></i>`
-    task.innerHTML = content;
+
+    const taskContent = document.createElement('div');
+    const titleTask = document.createElement('span');
+    titleTask.classList.add('task');
+    titleTask.innerText = input.value;
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
+
+    const content = `<i class="fas fa-trash-alt trashIcon icon"></i>`
+    //task.innerHTML = content;
 
     input.value = '';
-    list.appendChild(task)
+    task.appendChild(taskContent);
+    list.appendChild(task);
 }
 
 btn.addEventListener('click', crearTarea)
+
+const checkComplete = () => {
+  const i = document.createElement('i');
+  i.classList.add('far', 'fa-check-square', 'icon');
+  i.addEventListener('click', completeTask)
+  return i;
+}
+
+const completeTask = (event) => {
+  const element = event.target;
+  element.classList.toggle('fas');
+  element.classList.toggle('completeIcon');
+  element.classList.toggle('far');
+  console.log(event.target)
+}
+
+})();
