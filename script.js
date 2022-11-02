8aec17ea75cd61efe122a20a526294c9a46f4770
Jossueth <jossuetheldomador@hotmail.com>
2022-11-01 22:58:01 -0500

    Finalizando primeros pasos

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-( () => {
+  import checkComplete from "./components/checkComplete.js";
+  import deleteIcon from "./components/deleteIcon.js";
+  
+  const btn = document.querySelector("[data-form-btn]");
 
-const btn = document.querySelector("[data-form-btn]");
-
-const crearTarea = (evento) => {
+  const comprobarTarea = (evento) => {
     evento.preventDefault();
+    const word = document.getElementById("inputFormId").value;
+    const date = document.querySelector("[data-form-date]").value;
+    console.log(word);
+    //console.log(input.value)
+    const input = document.getElementById("inputFormId");
+    const datebox = document.querySelector("[data-form-date]");
+    if(word !== "" || date !== ""){
+      input.style="background-color: white; opacity: 1;"
+      datebox.style="background-color: white; opacity: 1;"
+      crearTarea();
+    }else{
+      input.style="background-color: red; opacity: .3;"
+      datebox.style="background-color: red; opacity: .3;"
+    }
+  }
+  
+  function crearTarea(){
     const input = document.querySelector("[data-form-imput]");
+    const date = document.querySelector("[data-form-date]");
+
+    console.log(date.value);
+    console.log(moment(date.value).format("DD/MM/YYYY"));
+    const data = new Date()
+    console.log(data)
+
     const list = document.querySelector('[data-list]');
     const task = document.createElement('li');
     task.classList.add('card');
@@ -16,29 +41,12 @@ const crearTarea = (evento) => {
     taskContent.appendChild(checkComplete());
     taskContent.appendChild(titleTask);
 
-    const content = `<i class="fas fa-trash-alt trashIcon icon"></i>`
-    //task.innerHTML = content;
 
+    //task.innerHTML = content;
     input.value = '';
     task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
     list.appendChild(task);
-}
+  }
 
-btn.addEventListener('click', crearTarea)
-
-const checkComplete = () => {
-  const i = document.createElement('i');
-  i.classList.add('far', 'fa-check-square', 'icon');
-  i.addEventListener('click', completeTask)
-  return i;
-}
-
-const completeTask = (event) => {
-  const element = event.target;
-  element.classList.toggle('fas');
-  element.classList.toggle('completeIcon');
-  element.classList.toggle('far');
-  console.log(event.target)
-}
-
-})();
+  btn.addEventListener('click', comprobarTarea)
