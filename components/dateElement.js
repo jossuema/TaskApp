7a2daed21cddcd2cffea75307a52af38fa459926
JosueMalla <negritomallapqs@hotmail.com>
2022-11-09 21:39:32 -0500

    Agregando fechas de tareas

---
+++ components/dateElement.js
@@ -0,0 +1,6 @@
+export default (date) => {
+    const dateText = document.createElement('li');
+    dateText.classList.add("dateText");
+    dateText.innerHTML = date;
+    return dateText;
+}
--- components/storageTask.js
+++ components/storageTask.js
@@ -1,10 +1,14 @@
 import { crearTarea } from "./addTask.js";
+import dateElement from "./dateElement.js";
 
 const readTask = () => {
     const list = document.querySelector('[data-list]')
     const taskList = JSON.parse(localStorage.getItem("Tasks")) || [];
 
+
+
     taskList.forEach(task => {
+        list.appendChild(dateElement(task.fecha));
         list.appendChild(crearTarea(task));
     });
 }
